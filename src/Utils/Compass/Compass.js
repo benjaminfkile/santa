02e3b1d069eca94f1617f99c250340c3aa4a3180
benjaminfkile <benjaminfkile@gmail.com>
2021-09-27 19:34:59 -0600

    try compass with react orientation package

--- src/Utils/Compass/Compass.js
+++ src/Utils/Compass/Compass.js
@@ -10,89 +10,97 @@
  * @flow
  */
 
- import React from 'react';
- import styleNormalizer from 'react-style-normalizer';
- import 'react-compass/src/styles.scss';
- import "./Compass.css"
- 
- export default class Compass extends React.Component {
-     static defaultProps = {
-         direction: 0,
-         directionNames: ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'],
-         theme: "night"
-     };
- 
-     constructor(props) {
-         super(props);
- 
-         this.oldAngle = 0;
- 
-     }
- 
-     directionName(dir) {
-         let sections = this.props.directionNames.length,
-             sect = 360 / sections,
-             x = Math.floor((dir + sect / 2) / sect);
- 
-         x = x >= sections ? 0 : x;
- 
-         return this.props.directionNames[x];
-     }
- 
-     normalizeAngle(direction) {
-         let newAngle = direction,
-             rot = this.oldAngle || 0,
-             ar = rot % 360;
- 
-         while (newAngle < 0) {
-             newAngle += 360;
-         }
-         while (newAngle > 360) {
-             newAngle -= 360;
-         }
-         while (rot < 0) {
-             rot += 360;
-         }
-         while (rot > 360) {
-             rot -= 360;
-         }
- 
-         if (ar < 0) {
-             ar += 360;
-         }
-         if (ar < 180 && newAngle > ar + 180) {
-             rot -= 360;
-         }
-         if (ar >= 180 && newAngle <= ar - 180) {
-             rot += 360;
-         }
- 
-         rot += newAngle - ar;
-         this.oldAngle = rot;
- 
-         return rot;
-     }
- 
-     render() {
-         let dir = this.normalizeAngle(this.props.direction),
-             name = this.directionName(dir);
- 
-         return (
-             <div className="compass" style={this.props.style} id={`compass-${this.props.theme.toLowerCase()}`}>
-                 <div className="compass__windrose"
-                      style={styleNormalizer({transform: `rotate(-${this.props.direction}deg)`})}>
-                     {[...Array(10)].map((k, i) => <div className="compass__mark" key={i + 1}></div>)}
-                     <div className="compass__mark--direction-h"></div>
-                     <div className="compass__mark--direction-v"></div>
-                 </div>
-                 <div className="compass__arrow-container">
-                     <div className="compass__arrow"></div>
-                     <div className="compass__labels">
-                         <span>{name}</span>
-                         <span>{Math.trunc(dir)}<sup>o</sup></span>
-                     </div>
-                 </div>
-             </div>
-         );
-     }
- }
+import React from 'react';
+import styleNormalizer from 'react-style-normalizer';
+import DeviceOrientation from 'react-device-orientation';
+import 'react-compass/src/styles.scss';
+import "./Compass.css"
+
+export default class Compass extends React.Component {
+    static defaultProps = {
+        direction: 0,
+        directionNames: ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'],
+        theme: "night"
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.oldAngle = 0;
+
+    }
+
+    directionName(dir) {
+        let sections = this.props.directionNames.length,
+            sect = 360 / sections,
+            x = Math.floor((dir + sect / 2) / sect);
+
+        x = x >= sections ? 0 : x;
+
+        return this.props.directionNames[x];
+    }
+
+    normalizeAngle(direction) {
+        let newAngle = direction,
+            rot = this.oldAngle || 0,
+            ar = rot % 360;
+
+        while (newAngle < 0) {
+            newAngle += 360;
+        }
+        while (newAngle > 360) {
+            newAngle -= 360;
+        }
+        while (rot < 0) {
+            rot += 360;
+        }
+        while (rot > 360) {
+            rot -= 360;
+        }
+
+        if (ar < 0) {
+            ar += 360;
+        }
+        if (ar < 180 && newAngle > ar + 180) {
+            rot -= 360;
+        }
+        if (ar >= 180 && newAngle <= ar - 180) {
+            rot += 360;
+        }
+
+        rot += newAngle - ar;
+        this.oldAngle = rot;
+
+        return rot;
+    }
+
+    render() {
+        let dir = this.normalizeAngle(this.props.direction),
+            name = this.directionName(dir);
+
+        return (
+            <div>
+                <DeviceOrientation>
+                    {({ absolute, alpha, beta, gamma }) => (
+                        <div className="compass" style={this.props.style} id={`compass-${this.props.theme.toLowerCase()}`}>
+                            {alpha}
+                            <div className="compass__windrose"
+                                style={styleNormalizer({ transform: `rotate(-${alpha}deg)` })}>
+                                {[...Array(10)].map((k, i) => <div className="compass__mark" key={i + 1}></div>)}
+                                <div className="compass__mark--direction-h"></div>
+                                <div className="compass__mark--direction-v"></div>
+                            </div>
+                            <div className="compass__arrow-container">
+                                <div className="compass__arrow"></div>
+                                <div className="compass__labels">
+                                    <span>{name}</span>
+                                    <span>{alpha}<sup>o</sup></span>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </DeviceOrientation>
+            </div>
+        );
+    }
+}
