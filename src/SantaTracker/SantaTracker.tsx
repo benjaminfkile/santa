23653129e3bc70c7da03cb5593df357d67b8d089
benjaminfkile <benjaminfkile@gmail.com>
2021-11-07 17:01:17 -0700

    bump for new production site

--- src/SantaTracker/SantaTracker.tsx
+++ src/SantaTracker/SantaTracker.tsx
@@ -13,7 +13,7 @@ type SantaTrackerTypes = {
     returnUser: boolean
 }
 
-class SantaTracker extends Component<SantaTrackerProps, SantaTrackerTypes> {
+class SantaTracker extends Component<SantaTrackerProps, SantaTrackerTypes> {//bump
 
     state = {
         preShow: true,
