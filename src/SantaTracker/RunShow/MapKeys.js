343d73181f1dde27281d7855b5f13dec62dc4bc5
benjaminfkile <benjaminfkile@gmail.com>
2021-11-09 16:54:43 -0700

    change api key for maps

--- src/SantaTracker/RunShow/MapKeys.js
+++ src/SantaTracker/RunShow/MapKeys.js
@@ -1,5 +1,5 @@
 let MapKey = {
     // mapUrl: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
-    mapUrl: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDCWWJLRXoNkdEhcpgJbmnkoWI-8tOgl10",
+    mapUrl: `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_MAPS_API_KEY}`,
 }
 export default MapKey
