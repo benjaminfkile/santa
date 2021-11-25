b87d6af1418999812bb0126cb71788176b41296a
benjaminfkile <benjaminfkile@gmail.com>
2021-11-24 19:45:39 -0700

    add donation letter btn

--- src/DonateSection/DonateSection.tsx
+++ src/DonateSection/DonateSection.tsx
@@ -9,8 +9,8 @@ import "./DonateSection.css"
 
 class DonateSection extends Component {
 
-    openLink = () => {
-        window.open("https://donations.missouladowntown.com/", '_blank')
+    openLink = (url: string) => {
+        window.open(url, '_blank')
     }
 
 
@@ -31,10 +31,14 @@ class DonateSection extends Component {
                     <p>
                         We depend on donations to pay for the cost of the flight, the cost of storing Santa, and the cost of materials such as lights and generators. 100% of all donations go toward funding Santa’s flight. We have partnered with the Missoula Downtown Foundation so that any donation made is tax-deductible. To donate, please click the Donate button below to be redirected to the Missoula Downtown Foundation donation page and select “Santa Flyover” in the “Project” drop-down menu.
                     </p>
+                    <div className="DonateSectionDonateLetter">
+                        <Button variant="secondary" id="donate-section-donate-btn" onClick={() => this.openLink("https://www.406santa.com/docs/WMSFO%20Donation%20Letter.pdf?fbclid=IwAR3qMCWcNUJflYBNdA5dhYohdYS9FdZIm-PqyCdYjaJKsSAEtPli7bt8jvM")}>Donation Letter</Button>
+                    </div>
                 </div>
                 <div className="DonateSectionDonateBtn">
-                    <Button id="donate-section-donate-btn" onClick={() => this.openLink()}>Donate</Button>
+                    <Button id="donate-section-donate-btn" onClick={() => this.openLink("https://donations.missouladowntown.com/")}>Donate</Button>
                 </div>
+
             </div>
         )
     }
