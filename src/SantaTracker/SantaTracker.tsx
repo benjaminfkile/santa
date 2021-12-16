3050539f9fd10e18d83675f4176ee7f1d3d16070
benjaminfkile <benjaminfkile@gmail.com>
2021-12-15 18:32:01 -0700

    uncomment mode lock

--- src/SantaTracker/SantaTracker.tsx
+++ src/SantaTracker/SantaTracker.tsx
@@ -52,7 +52,7 @@ class SantaTracker extends Component<SantaTrackerProps, SantaTrackerTypes> {
             mode = santaDat.mode + ""
         }
 
-        mode = "1"
+        // mode = "1"
 
         return (
             <div className="SantaTracker">
