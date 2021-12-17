6ddc1e80744297e13929538f1392c1d42b9d11e6
benjaminfkile <benjaminfkile@gmail.com>
2021-12-16 20:30:28 -0700

    add missing paragraph to preshow

--- src/SantaTracker/PreShow/PreShow.tsx
+++ src/SantaTracker/PreShow/PreShow.tsx
@@ -22,7 +22,7 @@ class PreShow extends Component<PreShowProps, {}> {
                 <div className="preshow-info-card">
                     <div className="preshow-info-card-header">
                         <p id="preshow-info-card-header-p1">Looks like Santa is still at the North Pole.</p>
-                        {/* <p id="preshow-info-card-header-p2">When he starts to move you can interact with the map.</p> */}
+                        <p id="preshow-info-card-header-p2">When he starts to move you can interact with the map.</p>
                     </div>
                     <div className="preshow-info-card-message">
                         {!message && <p>When he is on the move you can come back here, and it will turn into a tracker to help you locate him.</p>}
