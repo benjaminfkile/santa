2a67548654a06c7fb1bfbbedebc5307fb1f651fb
Ben Kile <ben.f.kile@elmutility.com>
2021-12-17 12:42:18 -0700

    new location service ogic

--- src/Utils/UserLocation/UserLocation.tsx
+++ src/Utils/UserLocation/UserLocation.tsx
@@ -1,25 +1,36 @@
+import snackBar from "../snackBar/SnackBar";
+
 let userLocation = {
     coordinates: { lat: null, lng: null },
     disable: true,
-    userDenied: false,
     getUserLocation: () => {
+        if (navigator.geolocation && !userLocation.disable) {
+            navigator.geolocation.getCurrentPosition(userLocation.showPosition, userLocation.showError)
+            userLocation.disable = false
+        } else {
+            // snackBar({ type: "error", text: "Geolocation is not supported by this browser.", timeout: 3000 })
+        }
+    },
 
-        if (!userLocation.disable) {
-            navigator.geolocation.getCurrentPosition(userLocation.showPosition, userLocation.errorCallback,
-                {
-                    maximumAge: Infinity,
-                    timeout: 0
-                })
-        }
-    },
-    errorCallback: (err: any) => {
-        if (err.code === err.PERMISSION_DENIED) {
-            userLocation.disable = true
-            userLocation.userDenied = true
-        }
-    },
     showPosition: (position: any) => {
         userLocation.coordinates = { lat: position.coords.latitude, lng: position.coords.longitude }
+    },
+    showError(error: any) {
+        userLocation.disable = true
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                snackBar({ type: "error", text: "User denied the request for Geolocation.", timeout: 3000 })
+                break;
+            case error.POSITION_UNAVAILABLE:
+                snackBar({ type: "error", text: "Location information is unavailable.", timeout: 3000 })
+                break;
+            case error.TIMEOUT:
+                snackBar({ type: "error", text: "The request to get user location timed out.", timeout: 3000 })
+                break;
+            case error.UNKNOWN_ERROR:
+                snackBar({ type: "error", text: "An unknown error occured", timeout: 3000 })
+                break;
+        }
     },
     inApp: () => {
         //@ts-ignore
