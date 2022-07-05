2d11d113ec7a51ebc570839066444fe4e9c87b04
ben <benjaminfkile>
2022-07-05 12:39:11 -0600

    bump

--- src/AboutSection/AboutSection.tsx
+++ src/AboutSection/AboutSection.tsx
@@ -3,12 +3,12 @@ import JumpingElf from "../Utils/JumpingElf/JumpingElf"
 import NavMenu from "../NavMenu/NavMenu"
 import Ornaments from "../Utils/Ornaments/Ornaments"
 import Mistletoe from "../SVG/Mistletoe/Mistletoe"
-import "./AboutSection.css"
 import Logo from "../Utils/Logo/Logo"
+import "./AboutSection.css"
 
 class AboutSection extends Component {
 
-    render() {//bump 
+    render() {
         return (
             <div className="AboutSection">
                 <NavMenu />
@@ -19,17 +19,33 @@ class AboutSection extends Component {
                 </div>
                 <div className="AboutContent">
                     <p>
-                        Western Montana Santa Flyover, with the help of the <a href="https://www.missouladowntown.com/" target="_blank" rel="noreferrer">Missoula Downtown Foundation</a>, is keeping the tradition of having Santa do a test flight over Missoula Mt. Hearing the kids, and adults, screaming “Santa” with cheer is what makes this flight a true Christmas Miracle.
+                        Western Montana Santa Flyover is a non-profit organization that, with the help of the Missoula Downtown Foundation,
+                        is keeping the tradition of having Santa do a flight over Missoula Mt.
+                        Throughout the years this flight has evolved and is now known as Santa’s Dash
+                        Through the Big Sky Flyover. This flyover will take place several days before
+                        Christmas Eve and will only happen ONCE. Updates about this flyover can be found on our <a href="https://www.facebook.com/groups/374773957140495/user/100073599618363" target="_blank" rel="noreferrer">Official Facebook page</a>.
+                    </p>
+                    <p>
+                        Santa’s Dash Through the Big Sky Flyover is only possible with the help of our sponsors and community.
+                        To see how you can help, you can click <a href="/donate">here</a> to be directed to the donation page. Hearing the kids, and adults,
+                        screaming “Santa” with cheer is what makes this flight a true Christmas Miracle.
                     </p>
                     <div className="AboutSectionContentImg1">
                         <img id="idfk" src="/res/mdf-logo.png" alt="" />
                         {/* <img src="https://i.ibb.co/QrCcpqC/e353718c-01b2-46dc-b52a-7853c976bf66-750x422.jpg" alt="Santa" />
                         <p id="img-credit-about-i-1">Credit: Getty Images/iStockphoto</p> */}
                     </div>
+                    <h3>
+                        How the Western Montana Santa Flyover organization started:
+                    </h3>
                     <p>
-                        In 2020, after a rough year of lockdowns and school closures, a group of Missoula residents started a
-                        Facebook group to get more people interested in seeing Santa fly again. The group grew rapidly and
-                        received a tremendous amount of support throughout the community. With the help of the <a href="https://www.missouladowntown.com/" target="_blank" rel="noreferrer">Missoula Downtown Foundation</a>, volunteers and from your donations, Santa flew over Missoula for the first time in over a decade. This group's plan is to continue this tradition of bringing joy and Christmas spirit to Western Montana for years to come.</p>
+                        In 2020, after a rough year of lockdowns and school closures, a group of Missoula residents
+                        started a Facebook group to get more people interested in seeing Santa fly again. The group
+                        grew rapidly and received a tremendous amount of support throughout the community. With
+                        the help of the Missoula Downtown Foundation, volunteers and from your donations, Santa
+                        flew over Missoula for the first time in over a decade. This group's plan is to continue this
+                        tradition of bringing joy and Christmas spirit to Western Montana for years to come.
+                    </p>
                     <div className="AboutContentLowerItem">
                         <div className="AboutContentLowerItemHeader">
                             <Mistletoe id="about-lower-content-mistltoe" />
