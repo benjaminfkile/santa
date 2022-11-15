94d879f8c84c1461906173a61d217121dbca188e
unknown <benjaminfkile@gmail.com>
2022-11-15 01:00:00 -0700

    bump

--- src/SantaTracker/PreShow/PreShow.tsx
+++ src/SantaTracker/PreShow/PreShow.tsx
@@ -1,5 +1,4 @@
 import { Component } from "react"
-import NavMenu from "../../NavMenu/NavMenu"
 import LaughingSanta from "../../Utils/LaughingSanta/LaughingSanta"
 import Snow from "../../Utils/Snow/Snow"
 import "./PreShow.css"
@@ -10,23 +9,24 @@ interface PreShowProps {
 
 class PreShow extends Component<PreShowProps, {}> {
 
-    render() {
+    render() {//bump
 
         let message = this.props.santaDat.message || null
 
         return (
             <div className="PreShow">
                 <Snow />
-                <NavMenu/>
+                <div className="SemiSantaHeader">
+                    <p>Semi Santa</p>
+                </div>
                 <div className="preshow-info-card">
                     <div className="preshow-info-card-header">
-                        <p id="preshow-info-card-header-p1">Looks like Santa is still at the North Pole.</p>
-                        <p id="preshow-info-card-header-p2">When he starts to move you can interact with the map.</p>
+                        <p id="preshow-info-card-header-p1">Semi-truck Santa’s scheduled to come!</p>
+                        {/* <p id="preshow-info-card-header-p2">When he starts to move you can interact with the map.</p> */}
                     </div>
                     <div className="preshow-info-card-message">
                         {!message && <p>When he is on the move you can come back here, and it will turn into a tracker to help you locate him.</p>}
                         {message && <p>{message}</p>}
-
                     </div>
                     <div className="preshow-info-card-laughing-santa-wrapper">
                         <LaughingSanta
