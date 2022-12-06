8dae496ea7b8617331ab4b7a950b850e2c41c5ad
unknown <benjaminfkile@gmail.com>
2022-12-05 19:14:20 -0700

    d09f6bd6-c170-48c6-b2d7-ab67efd43000

--- src/SantaTracker/PreShow/PreShow.tsx
+++ src/SantaTracker/PreShow/PreShow.tsx
@@ -16,13 +16,10 @@ class PreShow extends Component<PreShowProps, {}> {
         return (
             <div className="PreShow">
                 <Snow />
-                <div className="SemiSantaHeader">
-                    <p>Semi Santa</p>
-                </div>
                 <div className="preshow-info-card">
                     <div className="preshow-info-card-header">
-                        <p id="preshow-info-card-header-p1">Semi-truck Santa’s scheduled to come!</p>
-                        {/* <p id="preshow-info-card-header-p2">When he starts to move you can interact with the map.</p> */}
+                        <p id="preshow-info-card-header-p1">Looks like Santa is still at the North Pole.</p>
+                        <p id="preshow-info-card-header-p2">When he starts to move you can interact with the map.</p>
                     </div>
                     <div className="preshow-info-card-message">
                         {!message && <p>When he is on the move you can come back here, and it will turn into a tracker to help you locate him.</p>}
