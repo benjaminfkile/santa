c5c15a7fe520f642fc2665c798663dd14732be2d
unknown <benjaminfkile@gmail.com>
2022-12-15 17:39:39 -0700

    pull main, analytics bug fix

--- src/SantaTracker/SantaTracker.tsx
+++ src/SantaTracker/SantaTracker.tsx
@@ -55,7 +55,8 @@ class SantaTracker extends Component<SantaTrackerProps, SantaTrackerTypes> {
             mode = santaDat.mode + ""
         }
 
- 
+        mode = "1"
+        
         return (
             <div className="SantaTracker">
                 {!loading && santaDat && mode === "0" && <div className="PreShow">
