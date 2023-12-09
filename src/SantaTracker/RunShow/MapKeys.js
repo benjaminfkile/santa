c27ed46b981e25c5271c6fbcb1a7add7fb12f671
unknown <benjaminfkile@gmail.com>
2023-12-08 18:15:37 -0700

    add google map callback noop function

--- src/SantaTracker/RunShow/MapKeys.js
+++ src/SantaTracker/RunShow/MapKeys.js
@@ -1,5 +1,6 @@
 let MapKey = {
-    // mapUrl: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
-    mapUrl: `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_MAPS_API_KEY}`,
+    // mapUrl: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+    // mapUrl: `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_MAPS_API_KEY}`
+    mapUrl: `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_MAPS_API_KEY}&callback=Function.prototype`
 }
 export default MapKey
