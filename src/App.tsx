6f177dd6f7b5343060c76d44f8b42320e7ae32f5
Ben Kile <benjaminfkile@gmail.com>
2025-10-30 20:16:49 -0500

    add fallback urls

--- src/App.tsx
+++ src/App.tsx
@@ -1,112 +1,128 @@
-import { Component } from "react"
-import { Route, Switch } from "react-router-dom"
-import axios from "axios"
-import SantaTracker from "./SantaTracker/SantaTracker"
-import AboutSection from "./AboutSection/AboutSection"
-import DonateSection from "./DonateSection/DonateSection"
-import FundStatus from "./FundStatus/FundStatus"
-import SponsorsSection from "./SponsorsSection/SponsorsSection"
-import ContactSection from "./ContactSection/ContactSection"
-import 'bootstrap/dist/css/bootstrap.min.css'
-import fundData from "./Utils/FundsRing/FundData"
-import SponsorTypes from "./SponsorsSection/SponsorTypes"
+import { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import axios from "axios";
+import SantaTracker from "./SantaTracker/SantaTracker";
+import AboutSection from "./AboutSection/AboutSection";
+import DonateSection from "./DonateSection/DonateSection";
+import FundStatus from "./FundStatus/FundStatus";
+import SponsorsSection from "./SponsorsSection/SponsorsSection";
+import ContactSection from "./ContactSection/ContactSection";
+import fundData from "./Utils/FundsRing/FundData";
+import SponsorTypes from "./SponsorsSection/SponsorTypes";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 type AppTypes = {
-  santaDat: any
-  sponsors: Array<SponsorTypes> | any
-}
+  santaDat: any;
+  sponsors: Array<SponsorTypes> | any;
+};
 
-class App extends Component<{}, AppTypes>{
-
+class App extends Component<{}, AppTypes> {
   state = {
     santaDat: null,
-    sponsors: []
-  }
+    sponsors: [],
+  };
 
-  updateFrequency = 5000
-  getSantaInterval: any
+  updateFrequency = 5000;
+  getSantaInterval: any;
+  usingFallbackSanta = false; // switches permanently after first primary failure
 
   componentDidMount() {
-    this.getSanta()
-    this.getSponosrs()
-    fundData.getFundData()
-    //eslint-disable-next-line
-    console.log("\n  .-\"\"-.\r\n \/,..___\\\r\n() {_____}\r\n  (\/-@-@-\\)\r\n  {`-=^=-\'}\r\n  {  `-\'  }\r\n   {     }\r\n    `---\'\n\nDeveloped by Ben Kile\n\n")
+    this.getSanta();
+    this.getSponsors();
+    fundData.getFundData();
+    // eslint-disable-next-line
+    console.log(
+      "\n  .-\"\"-.\r\n \\/,..___\\\r\n() {_____}\r\n  (\\/-@-@-\\)\r\n  {`-=^=-'}\r\n  {  `-'  }\r\n   {     }\r\n    `---'\n\nDeveloped by Ben Kile\n\n"
+    );
   }
 
   componentWillUnmount() {
-    clearInterval(this.getSantaInterval)
+    clearTimeout(this.getSantaInterval);
   }
 
-  getSanta = () => {
-    // console.log("getting santa every " + this.updateFrequency + " ms")
-    axios.get(`${process.env.REACT_APP_WMSFO_LOCATION_DATA_API_URL}/api/location-data`)
-      .then(res => {
-        if (res.data) {
-          this.setState({ santaDat: res.data })
+  /** Fetches Santa data every 5 seconds. Once the primary fails, permanently switches to fallback. */
+  getSanta = async () => {
+    const primary = process.env.REACT_APP_WMSFO_LOCATION_DATA_API_URL!;
+    const fallback = process.env.REACT_APP_WMSFO_LOCATION_DATA_API_URL_FALLBACK;
+
+    const activeUrl = this.usingFallbackSanta && fallback ? fallback : primary;
+
+    try {
+      const res = await axios.get(`${activeUrl}/api/location-data`);
+      this.setState({ santaDat: res.data });
+    } catch (err) {
+      // Switch to fallback permanently if primary fails
+      if (!this.usingFallbackSanta && fallback) {
+        console.warn(
+          `Primary Santa URL failed (${primary}), switching permanently to fallback: ${fallback}`
+        );
+        this.usingFallbackSanta = true;
+      } else {
+        console.error("Failed to fetch Santa data:", err);
+      }
+    } finally {
+      clearTimeout(this.getSantaInterval);
+      this.getSantaInterval = setTimeout(this.getSanta, this.updateFrequency);
+    }
+  };
+
+  /** Fetch sponsors once (tries primary, falls back if needed) */
+  getSponsors = async () => {
+    const primary = `${process.env.REACT_APP_MRS_CLAUS_API_URL}/api/sponsors/get-sponsors`;
+    const fallback = process.env.REACT_APP_MRS_CLAUS_API_URL_FALLBACK
+      ? `${process.env.REACT_APP_MRS_CLAUS_API_URL_FALLBACK}/api/sponsors/get-sponsors`
+      : undefined;
+
+    try {
+      const res = await axios.get(primary);
+      this.setState({ sponsors: res.data });
+    } catch (err) {
+      if (fallback) {
+        console.warn(
+          `Primary sponsors URL failed (${primary}), trying fallback: ${fallback}`
+        );
+        try {
+          const res = await axios.get(fallback);
+          this.setState({ sponsors: res.data });
+        } catch (fallbackErr) {
+          console.error("Both sponsor URLs failed:", fallbackErr);
         }
-        if (res.data.throttle !== this.updateFrequency) {
-          clearInterval(this.getSantaInterval)
-          this.updateFrequency = res.data.throttle * 1000
-          this.getSantaInterval = setInterval(this.getSanta, this.updateFrequency)
-        }
-      })
-  }
-
-  getSponosrs = () => {
-    axios.get(`${process.env.REACT_APP_MRS_CLAUS_API_URL}/api/sponsors/get-sponsors`)
-      .then(res => {
-        this.setState({ sponsors: res.data })
-      }).catch(err => {
-        console.log(err)
-      })
-  }
-
+      } else {
+        console.error("Failed to fetch sponsors:", err);
+      }
+    }
+  };
 
   render() {
-
     return (
       <div className="WimsfoSanta">
         <Switch>
+          <Route exact path="/" render={() => <AboutSection />} />
+          <Route path="/about" render={() => <AboutSection />} />
+          <Route path="/donate" render={() => <DonateSection />} />
+          <Route path="/funding" render={() => <FundStatus />} />
           <Route
-            exact path='/'
-            render={() => <AboutSection />}
+            path="/santa"
+            render={() => (
+              <SantaTracker
+                santaDat={this.state.santaDat}
+                sponsors={this.state.sponsors}
+              />
+            )}
           />
           <Route
-            path='/about'
-            render={() => <AboutSection />}
+            path="/sponsors"
+            render={() => (
+              <SponsorsSection sponsors={this.state.sponsors} />
+            )}
           />
-          <Route
-            path='/donate'
-            render={() => <DonateSection />}
-          />
-          <Route
-            path='/funding'
-            render={() => <FundStatus />}
-          />
-          <Route
-            path='/santa'
-            render={() => <SantaTracker
-              santaDat={this.state.santaDat}
-              sponsors={this.state.sponsors}
-            />}
-          />
-          <Route
-            path='/sponsors'
-            render={() => <SponsorsSection
-              sponsors={this.state.sponsors}
-            />}
-          />
-          <Route
-            path='/contact'
-            render={() => <ContactSection />}
-          />
+          <Route path="/contact" render={() => <ContactSection />} />
         </Switch>
         <div id="snackbar">snacks</div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/Utils/FundsRing/FundData.tsx
+++ src/Utils/FundsRing/FundData.tsx
@@ -1,13 +1,32 @@
-import axios from "axios"
-let fundData = {
+import axios from "axios";
+
+let usingFallbackFund = false;
+
+const fundData = {
     percent: -1,
-    getFundData: () => {
-        axios.get(`${process.env.REACT_APP_MRS_CLAUS_API_URL}/api/funds/get-fund-status`)
-            .then(res => {
-                if (res.data.percent) {
-                    fundData.percent = res.data.percent
-                }
-            })
-    }
-}
-export default fundData
+
+    async getFundData() {
+        const primary = process.env.REACT_APP_MRS_CLAUS_API_URL!;
+        const fallback = process.env.REACT_APP_MRS_CLAUS_API_URL_FALLBACK;
+
+        const activeUrl = usingFallbackFund && fallback ? fallback : primary;
+
+        try {
+            const res = await axios.get(`${activeUrl}/api/funds/get-fund-status`);
+            if (res.data?.percent !== undefined) {
+                fundData.percent = res.data.percent;
+            }
+        } catch (err) {
+            if (!usingFallbackFund && fallback) {
+                console.warn(
+                    `Primary funds URL failed (${primary}), switching permanently to fallback: ${fallback}`
+                );
+                usingFallbackFund = true;
+            } else {
+                console.error("Failed to fetch fund data:", err);
+            }
+        }
+    },
+};
+
+export default fundData;
